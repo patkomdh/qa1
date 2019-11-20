a5f988776fad51ce4ec9b081972a950c38b7c6c7
student <patkomdh@gmail.com>
2019-11-20 09:01:49 +0100

    lll

---
+++ qagui/src/test/java/com/jsystems/qa/qagui/ConfigFrontend.java
@@ -0,0 +1,79 @@
+package com.jsystems.qa.qagui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+public class ConfigFrontend {
+    protected WebDriver driver;
+
+    //    String chromePath;
+    String fireFoxPath;
+    {
+        try {
+//            chromePath = Paths.get(getClass().getClassLoader().getResource("drivers/chromedriver.exe")
+//                    .toURI()).toFile().getAbsolutePath();
+            fireFoxPath = Paths.get(getClass().getClassLoader().getResource("drivers/geckodriver.exe")
+                    .toURI()).toFile().getAbsolutePath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    public void setUpEach() throws MalformedURLException {
+        setupSystemProperties();
+
+        driver = new FirefoxDriver();
+
+        setupDriver();
+    }
+
+    @AfterEach
+    public void tearDownEach() {
+        driver.quit();
+    }
+
+    private void setupSystemProperties() {
+//        System.setProperty("webdriver.chrome.driver", chromePath);
+        System.setProperty("webdriver.gecko.driver", fireFoxPath);
+    }
+
+    private void setupDriver() {
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+    }
+
+    private void setUpRemote() {
+        //        DesiredCapabilities cap = DesiredCapabilities.chrome();
+//        cap.setPlatform(Platform.LINUX);
+//        cap.setVersion("");
+//
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.setCapability("platform", "LINUX");
+//        options.setCapability("browserName", "chrome");
+//        driver = null;
+//        try {
+//            driver = new RemoteWebDriver(new URL("http://10.0.75.1:4444/wd/hub"), cap);
+//        setupDriver();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
---
+++ qagui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
@@ -0,0 +1,83 @@
+package com.jsystems.qa.qagui;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FrontendTest extends ConfigFrontend {
+
+    @Test
+    public void frontTest() {
+        driver.get("https://wordpress.com/");
+        WebElement textElement_1 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(1)"));
+        String text1 = textElement_1.getText();
+        assertTrue(text1.equals("WordPress powers"));
+
+        WebElement textElement_2 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(2)"));
+        String text2 = textElement_2.getText();
+        assertTrue(text2.contains("% of the internet."));
+        assertThat(text2).matches("\\d+(% of the internet.)");
+
+    }
+
+    @Test
+    public void loginTest() {
+
+        driver.navigate().to("https://www.wordpress.com/");
+
+        String loginIconSelector = ".x-nav-item.x-nav-item--wide.x-nav-item--logged-in";
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(loginIconSelector)));
+
+        WebElement loginIcon = driver.findElement(By.cssSelector(loginIconSelector));
+        wait.until(ExpectedConditions.elementToBeClickable(loginIcon));
+
+        loginIcon.click();
+
+        String usernameOrEmailSelector = "usernameOrEmail";
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(usernameOrEmailSelector)));
+
+        WebElement usernameInput = driver.findElement(By.id(usernameOrEmailSelector));
+
+        usernameInput.clear();
+        usernameInput.sendKeys("testautomation112019@wp.pl");
+
+        String primaryButtonSelector = ".button.form-button.is-primary";
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+        WebElement usernameButton = driver.findElement(By.cssSelector(primaryButtonSelector));
+        usernameButton.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));
+        WebElement inputPassword = driver.findElement(By.id("password"));
+
+        inputPassword.clear();
+        inputPassword.sendKeys("testautomation");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+        WebElement buttonPassword = driver.findElement(By.cssSelector(primaryButtonSelector));
+        usernameButton.click();
+
+        String userAvatarSelector = ".masterbar__item.masterbar__item-me";
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(userAvatarSelector)));
+
+        WebElement userAvatar = driver.findElement(By.cssSelector(userAvatarSelector));
+        userAvatar.click();
+
+        String userDisplayNameSelector = ".profile-gravatar__user-display-name";
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(userDisplayNameSelector)));
+
+        WebElement userDisplayName = driver.findElement(By.cssSelector(userDisplayNameSelector));
+        String userDisplayNameText = userDisplayName.getText();
+
+        assertThat(userDisplayNameText).isEqualTo("testautomation112019");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(primaryButtonSelector)));
+        WebElement saveUserDetailsButton = driver.findElement(By.cssSelector(primaryButtonSelector));
+        assertThat(!saveUserDetailsButton.isDisplayed());
+    }
+
+}
